bd5b7832d451b34c69fd8ff7ce20772abcc1f922
Robert Wahler <robert@gearheadforhire.com>
2010-03-12 11:58:09 -0500

    updated spec for aruba

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'basic_app'
 require 'spec'
 require 'spec/autorun'
+require 'aruba/api'
+
+# aruba helper, returns to full path to files
+# in the aruba tmp folder
+def fullpath(filename)
+  File.expand_path(File.join(current_dir, filename))
+end
 
 Spec::Runner.configure do |config|
-  
+   config.include Aruba::Api
 end
