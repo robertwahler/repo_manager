010c1ff537f35dd9e6a517f011b00e747c9ef613
Robert Wahler <robert@gearheadforhire.com>
2011-11-09 14:34:42 -0500

    override aruba to avoid 'current_ruby' call
    and make sure that binaries run on Win32 without the binstubs

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -6,13 +6,10 @@ APP_BIN_PATH = File.join(FileUtils.pwd, 'bin', 'basic_app')
 module Aruba
   module Api
 
-   alias_method :old_detect_ruby, :detect_ruby
-
-    # override aruba
+    # override aruba avoid 'current_ruby' call and make sure
+    # that binary run on Win32 without the binstubs
     def detect_ruby(cmd)
       cmd = cmd.gsub(/^basic_app/, "ruby -S #{APP_BIN_PATH}")
-      # run original aruba 'detect_ruby'
-      old_detect_ruby(cmd)
     end
   end
 end
