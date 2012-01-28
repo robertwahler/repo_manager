726694216ccf1333d2e55b5e7102b833e7eb24a1
Robert Wahler <robert@gearheadforhire.com>
2012-01-28 10:27:58 -0500

    refactor core_spec to better reflect what is tested

--- spec/basic_app/core_spec.rb
+++ spec/basic_app/core_spec.rb
@@ -1,48 +1,47 @@
 require 'spec_helper'
 
-describe Array do
+describe "Core" do
 
-  describe 'recursively_symbolize_keys!' do
-
-    it "should recursively convert a hash with string keys to a hash with symbol keys" do
-      hash_symbols = {
-                        :options => {
-                          :verbose => false,
-                        },
-                          :repos => {
-                             :repo1 => {:path => "something"}
-                        }
+  before :each do
+    @hash_symbols = {
+                      :options => {
+                        :verbose => false,
+                      },
+                        :repos => {
+                           :repo1 => {:path => "something"}
                       }
+                    }
 
-      hash_strings = {
-                        'options' => {
-                          'verbose' => false,
-                        },
-                          'repos' => {
-                             'repo1' => {'path' => "something"}
-                        }
+    @hash_strings = {
+                      'options' => {
+                        'verbose' => false,
+                      },
+                        'repos' => {
+                           'repo1' => {'path' => "something"}
                       }
+                    }
+  end
 
-      hash_symbols.should == hash_strings.recursively_symbolize_keys!
+  describe Hash do
+
+    describe 'recursively_symbolize_keys!' do
+      it "should recursively convert a hash with string keys to a hash with symbol keys" do
+        @hash_symbols.should == @hash_strings.recursively_symbolize_keys!
+      end
+
+      it "should handle hashes that are already symbolized" do
+        hash_copy = @hash_symbols.dup
+        hash_copy.should == @hash_symbols.recursively_symbolize_keys!
+        @hash_symbols[:repos][:repo1].should == {:path => "something"}
+      end
     end
 
-    it "should should handle hashes that are already symbolized" do
-      hash_symbols = {
-                        :options => {
-                          :verbose => false,
-                        },
-                          :repos => {
-                             :repo1 => {:path => "something"}
-                        }
-                      }
-
-      hash_copy = hash_symbols.dup
-
-      hash_copy.should == hash_symbols.recursively_symbolize_keys!
-      hash_symbols[:repos][:repo1].should == {:path => "something"}
+    describe 'to_conf' do
+      it "should convert a hash to sorted YAML" do
+        pending "to_conf code"
+      end
     end
 
   end
-
 end
 
