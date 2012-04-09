c68baaae9167efdf59ad3575e063225dee2ea832
Robert Wahler <robert@gearheadforhire.com>
2012-04-09 16:49:43 -0400

    fix issue with strings that can end up in non UTF-8 encodings
    like ASCII-8BIT, force encoding so that they are not written to YAML as binary

--- spec/basic_app/core_spec.rb
+++ spec/basic_app/core_spec.rb
@@ -40,7 +40,9 @@ describe "Core" do
 
     describe 'to_conf' do
 
+      # Ruby 1.8
       if RUBY_VERSION =~ /^1.8/
+
         it "should convert a hash of symbolized keys to sorted YAML" do
           @hash_symbols.to_conf.should == "--- \n:options: \n  :verbose: false\n:repos: \n  :repo1: \n    :path: something\n:zebras: true\n"
         end
@@ -48,7 +50,10 @@ describe "Core" do
         it "should convert a hash of stringified keys to sorted YAML" do
           @hash_strings.to_conf.should == "--- \noptions: \n  verbose: false\nrepos: \n  repo1: \n    path: something\nzebras: true\n"
         end
+
+      # Ruby 1.9+
       else
+
         it "should convert a hash of symbolized keys to insertion order YAML" do
           @hash_symbols.to_conf.should == "---\n:zebras: true\n:options:\n  :verbose: false\n:repos:\n  :repo1:\n    :path: something\n"
         end
@@ -56,7 +61,16 @@ describe "Core" do
         it "should convert a hash of stringified keys to insertion order YAML" do
           @hash_strings.to_conf.should == "---\nzebras: true\noptions:\n  verbose: false\nrepos:\n  repo1:\n    path: something\n"
         end
+
+        it "should force string encoding to UTF-8" do
+          h = {:num => 2000, :str1 => "hello".force_encoding("UTF-8"), :str2 => "world".force_encoding("ASCII-8BIT")}
+          h.to_conf.match(/\!binary \|/).should_not be_true
+          h.to_conf.should == "---\n:num: 2000\n:str1: hello\n:str2: world\n"
+        end
+
       end
+
+
     end
 
   end
