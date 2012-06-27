9d56a727156ddaaf1cb49d3f8e9a984e5311b810
Robert Wahler <robert@gearheadforhire.com>
2012-06-27 10:48:31 -0400

    add back repoman requires to actions.rb

--- lib/repoman/actions.rb
+++ lib/repoman/actions.rb
@@ -3,3 +3,8 @@ require 'repoman/actions/app_action'
 require 'repoman/actions/help_action'
 require 'repoman/actions/list_action'
 require 'repoman/actions/task_action'
+require 'repoman/actions/path_action'
+require 'repoman/actions/git_action'
+require 'repoman/actions/init_action'
+require 'repoman/actions/status_action'
+
