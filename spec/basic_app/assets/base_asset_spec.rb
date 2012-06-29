a76556b68a7b038ff2cdd75f6d50dbe23ef4309c
Robert Wahler <robert@gearheadforhire.com>
2012-06-29 12:14:25 -0400

    add path, tags, description, notes to base_asset

--- spec/basic_app/assets/base_asset_spec.rb
+++ spec/basic_app/assets/base_asset_spec.rb
@@ -39,6 +39,80 @@ describe BasicApp::BaseAsset  do
     end
   end
 
+  describe 'attributes' do
 
+    before :each do
+      @asset = BasicApp::BaseAsset.new
+      @asset.name = "test_asset"
+    end
+
+    describe 'description' do
+
+      it "should be nil unless set" do
+        @asset.attributes[:description].should be_nil
+        @asset.description.should be_nil
+      end
+
+      it "should render mustache templates" do
+        @asset.description = "This is a {{name}}"
+        @asset.attributes[:description].should == "This is a {{name}}"
+        @asset.description.should == "This is a test_asset"
+      end
+
+    end
+
+    describe 'notes' do
+
+      it "should be nil unless set" do
+        @asset.attributes[:notes].should be_nil
+        @asset.notes.should be_nil
+      end
+
+      it "should render mustache templates" do
+        @asset.notes = "This is a {{name}}"
+        @asset.attributes[:notes].should == "This is a {{name}}"
+        @asset.notes.should == "This is a test_asset"
+      end
+
+    end
+
+    describe 'path' do
+
+      it "should be nil unless set" do
+        @asset.attributes[:path].should be_nil
+        @asset.path.should be_nil
+      end
+
+      it "should expand '~'" do
+        @asset.path = "~/test/here"
+        @asset.attributes[:path].should == "~/test/here"
+        @asset.path.should_not match(/^#{File.expand_path(FileUtils.pwd)}\/test\/here$/)
+        @asset.path.should match(/^\/.*\/test\/here$/)
+      end
+
+      it "should expand relative paths" do
+        @asset.path = "test/here"
+        @asset.attributes[:path].should == "test/here"
+        @asset.path.should match(/^#{File.expand_path(FileUtils.pwd)}\/test\/here$/)
+      end
+
+      it "should render mustache templates" do
+        @asset.path = "test/{{name}}/here"
+        @asset.attributes[:path].should == "test/{{name}}/here"
+        @asset.path.should match(/^#{File.expand_path(FileUtils.pwd)}\/test\/test_asset\/here$/)
+      end
+
+    end
+
+    describe 'tags' do
+
+      it "should be an empty array unless set" do
+        @asset.attributes[:tags].should be_nil
+        @asset.tags.should be_empty
+      end
+
+    end
+
+  end
 end
 
