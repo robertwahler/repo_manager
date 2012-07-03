ba99be6dfc33419dba3c806a9cb6bc7dff8eafd3
Robert Wahler <robert@gearheadforhire.com>
2012-07-03 10:57:32 -0400

    remove basic_app specific paths from action_helper spec
    Creates less hassle in clone merges

--- spec/basic_app/actions/action_helper_spec.rb
+++ spec/basic_app/actions/action_helper_spec.rb
@@ -12,14 +12,14 @@ describe BasicApp::ActionHelper  do
 
     it "should return relative path given an absolute path" do
       thing = Thing.new
-      FileUtils.stub!('pwd').and_return '/home/robert/workspace/basic_app'
-      thing.relative_path('/home/robert/workspace/basic_app/photos').should == './photos'
+      FileUtils.stub!('pwd').and_return '/home/robert/workspace/my_app'
+      thing.relative_path('/home/robert/workspace/my_app/photos').should == './photos'
       thing.relative_path('/home/robert/photos').should == '../../photos'
     end
 
     it "should return relative path given a relative path" do
       thing = Thing.new
-      FileUtils.stub!('pwd').and_return '/home/someuser/basic_app'
+      FileUtils.stub!('pwd').and_return '/home/someuser/my_app'
       thing.relative_path('cats/dogs').should == './cats/dogs'
     end
 
@@ -29,14 +29,14 @@ describe BasicApp::ActionHelper  do
 
     it "should return relative path given an absolute path" do
       thing = Thing.new
-      FileUtils.stub!('pwd').and_return 'c:/home/robert/workspace/basic_app'
-      thing.relative_path('c:/home/robert/workspace/basic_app/photos').should == './photos'
+      FileUtils.stub!('pwd').and_return 'c:/home/robert/workspace/my_app'
+      thing.relative_path('c:/home/robert/workspace/my_app/photos').should == './photos'
       thing.relative_path('c:/home/robert/photos').should == '../../photos'
     end
 
     it "should return relative path given a relative path" do
       thing = Thing.new
-      FileUtils.stub!('pwd').and_return 'c:/home/someuser/basic_app'
+      FileUtils.stub!('pwd').and_return 'c:/home/someuser/my_app'
       thing.relative_path('cats/dogs').should == './cats/dogs'
     end
 
