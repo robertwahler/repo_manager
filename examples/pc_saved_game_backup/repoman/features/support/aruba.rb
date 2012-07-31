5a17778d657e59014194fcdaab6e08eec14d0c3c
Robert Wahler <robert@gearheadforhire.com>
2012-07-31 16:19:31 -0400

    example project's features run on win32 using aruba

---
+++ examples/pc_saved_game_backup/repoman/features/support/aruba.rb
@@ -0,0 +1,16 @@
+require 'aruba/api'
+require 'fileutils'
+require 'repoman/test/test_api'
+
+module Aruba
+  module Api
+
+    # override aruba avoid 'current_ruby' call and make sure
+    # that binary run on Win32 without the binstubs
+    def detect_ruby(cmd)
+      wrapper = which('repo')
+      cmd = cmd.gsub(/^repo/, "ruby -S #{wrapper}") if wrapper
+      cmd
+    end
+  end
+end
